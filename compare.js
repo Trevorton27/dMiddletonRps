2bb471ac58f3505db6a9d6fdc6b7d0157d252abb
Trevor Mearns <spiral272@gmail.com>
2021-05-22 13:04:01 +0900

    ready for code review approval

--- compare.js
+++ compare.js
@@ -3,57 +3,54 @@ const pcChoiceArea = document.querySelector('#pc-choice');
 const pointerBox = document.querySelector('#pointer');
 const buttons = document.querySelector('#buttons');
 const body = document.querySelector('body');
-const outcomeWrapper = document.getElementById('outComeWrapper');
 let playerScore = 0;
 let pcScore = 0;
 
 function getPlayerChoice(element) {
   if (element.target.classList.contains('rock')) {
     return 'rock';
-  }
-  if (element.target.classList.contains('paper')) {
+  } else if (element.target.classList.contains('paper')) {
     return 'paper';
-  }
-  if (element.target.classList.contains('scissors')) {
+  } else if (element.target.classList.contains('scissors')) {
     return 'scissors';
   }
 }
 
-function getPcChoice() {
+function getPCChoice() {
   let decision = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
   return decision;
 }
 
 function createOutcome(playerChoice, pcChoice) {
-  switch (playerChoice + pcChoice) {
-    case 'rockscissors':
-    case 'paperrock':
-    case 'scissorspaper':
-      playerScore++;
-      return 'Congratulations, you win!';
-    case 'rockpaper':
-    case 'paperscissors':
-    case 'scissorsrock':
-      pcScore++;
-      return 'Sorry, you lost.';
-    case 'rockrock':
-    case 'paperpaper':
-    case 'scissorsscissors':
-      return 'Draw.';
+  if (
+    (playerChoice === 'rock' && pcChoice === 'scissors') ||
+    (playerChoice === 'paper' && pcChoice === 'rock') ||
+    (playerChoice === 'scissors' && pcChoice === 'paper')
+  ) {
+    return 'Congratulations, you win!';
+  } else if (
+    (playerChoice === 'rock' && pcChoice === 'paper') ||
+    (playerChoice === 'paper' && pcChoice === 'scissors') ||
+    (playerChoice === 'scissors' && pcChoice === 'rock')
+  ) {
+    return 'Sorry, you lost.';
+  } else if (
+    (playerChoice === 'rock' && pcChoice === 'rock') ||
+    (playerChoice === 'paper' && pcChoice === 'paper') ||
+    (playerChoice === 'scissors' && pcChoice === 'scissors')
+  ) {
+    return 'Draw.';
   }
 }
 
 function displayChoices(playerChoice, pcChoice) {
   playerChoiceArea.classList.add(`${playerChoice}`);
   pcChoiceArea.classList.add(`${pcChoice}`);
-  outcomeWrapper.className = 'displayOutcome';
 }
 
 function drawPlayerScore() {
   const playerScoreText = document.querySelector('#player-score');
-  if (playerScoreText.textContent !== '') {
-    playerScoreText.textContent = '';
-  }
+  playerScoreText.textContent = '';
   playerScoreText.appendChild(
     document.createTextNode(`Player: ${playerScore}`)
   );
@@ -61,9 +58,7 @@ function drawPlayerScore() {
 
 function drawPCScore() {
   const pcScoreText = document.querySelector('#pc-score');
-  if (pcScoreText.textContent !== '') {
-    pcScoreText.textContent = '';
-  }
+  pcScoreText.textContent = '';
   pcScoreText.appendChild(document.createTextNode(`Computer: ${pcScore}`));
 }
 
@@ -71,8 +66,6 @@ function createResetButton() {
   const resetButton = document.createElement('button');
   resetButton.classList.add('reset');
   resetButton.classList.add('shrink');
-  resetButton.textContent = 'Play again?';
-  resetButton.addEventListener('click', resetBoard);
   return resetButton;
 }
 
@@ -81,21 +74,25 @@ function resetBoard(element) {
     playerChoiceArea.classList.remove('rock', 'paper', 'scissors');
     pcChoiceArea.classList.remove('rock', 'paper', 'scissors');
     pointerBox.classList.remove('left-arrow', 'right-arrow', 'cross');
-    outcomeWrapper.innerHTML = '';
+    body.removeChild(document.querySelector('h2'));
   }
 }
 
 function drawPointer(outcome) {
   if (outcome === 'Congratulations, you win!') {
     pointerBox.classList.add('left-arrow');
-  }
-
-  if (outcome === 'Sorry, you lost.') {
+  } else if (outcome === 'Sorry, you lost.') {
     pointerBox.classList.add('right-arrow');
-  }
-
-  if (outcome === 'Draw.') {
+  } else if (outcome === 'Draw.') {
     pointerBox.classList.add('cross');
+  }
+}
+
+function addPoint(outcome) {
+  if (outcome === 'Congratulations, you win!') {
+    playerScore++;
+  } else if (outcome === 'Sorry, you lost.') {
+    pcScore++;
   }
 }
 
@@ -105,30 +102,37 @@ function drawScores() {
 }
 
 function createOutcomeTextbox(outcome) {
-  const outcomeText = document.createElement('h3');
-
-  outcomeWrapper.appendChild(outcomeText);
-  outcomeText.setAttribute('class', 'outComeText');
-  const playAgainButtonWrapper = document.createElement('div');
+  const outcomeText = document.createElement('h2');
   outcomeText.appendChild(document.createTextNode(outcome));
-  outcomeText.appendChild(playAgainButtonWrapper);
-  playAgainButtonWrapper.appendChild(createResetButton());
+  outcomeText.appendChild(createResetButton());
+  body.appendChild(outcomeText);
 }
 
 function drawOutcome(element) {
+  if (
+    playerChoiceArea.classList.contains('rock') ||
+    playerChoiceArea.classList.contains('paper') ||
+    playerChoiceArea.classList.contains('scissors')
+  ) {
+    return;
+  }
   const playerChoice = getPlayerChoice(element);
-  const pcChoice = getPcChoice();
+  const pcChoice = getPCChoice();
   const outcome = createOutcome(playerChoice, pcChoice);
+  if (playerChoice === undefined) {
+    return;
+  }
   displayChoices(playerChoice, pcChoice);
+  addPoint(outcome);
   drawPointer(outcome);
   createOutcomeTextbox(outcome);
 }
 
-function renderResults(element) {
+function handleClickEvent(element) {
   drawOutcome(element);
   drawScores();
-  drawPCScore();
 }
 
 drawScores();
-buttons.addEventListener('click', renderResults);
+buttons.addEventListener('click', handleClickEvent);
+body.addEventListener('click', resetBoard);
